66d67f93e05570351e29b32850d427ade972c0a5
sinabdeev <v.sinabdeev@mail.ru>
2017-09-19 21:49:00 +0400

    first commit

---
+++ version_updater/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using version_updater.Properties;
+using System.Configuration;
+using System.Reflection;
+
+namespace version_updater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region Обновлялка
+            // проверяем обновилась ли версия
+            try
+            {
+                // смотрим инфу о текущей версии
+                long oldVersion = Settings.Default.Version;
+                // обновляем .config взятый с сервера
+                string fConfigName = Assembly.GetExecutingAssembly().ManifestModule.Name + ".config";
+                FileInfo fConfigServer = new FileInfo(Settings.Default.FConfigServer + fConfigName);
+                if (!fConfigServer.Exists) throw new FileNotFoundException(Resources.ErrFileConfigNotFoundServer, fConfigServer.FullName);
+                fConfigServer.CopyTo(fConfigName, true);
+                Settings.Default.Reload();
+                long newVersion = Settings.Default.Version;
+                if (newVersion == 0) throw new Exception(Resources.ErrFileEmptyServer);
+
+                foreach (string fileName in Settings.Default.CopyFileList)
+                {
+                    FileInfo fInfoServer = new FileInfo(Settings.Default.FConfigServer + fileName);
+                    FileInfo fInfoLocal = new FileInfo(fileName);
+                    // Если на серваке нет такого файла - ошибка
+                    if (!fInfoServer.Exists) throw new System.IO.FileNotFoundException(Resources.ErrFileNotFoundServer, fInfoServer.FullName);
+                    // Если локально такого файла нет - копируем
+                    if (!fInfoLocal.Exists)
+                    {
+                        string path = Path.GetDirectoryName(fileName);
+                        if (path != "")
+                        {
+                            DirectoryInfo dir = new DirectoryInfo(path);
+                            if (!dir.Exists) dir.Create();
+                        }
+                        fInfoServer.CopyTo(fileName, true);
+                        continue;
+                    }
+                    // Если дата изменения локального файла меньше - копируем
+                    if (fInfoLocal.LastWriteTime < fInfoServer.LastWriteTime) fInfoServer.CopyTo(fileName, true);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message + "\r\n" + ex.FileName, Resources.ErrText, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message, Resources.ErrText); }
+
+            // запускаем приложение
+            try
+            { System.Diagnostics.Process.Start(Settings.Default.AfterStartApplication); }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message + "\r\napp:" + Settings.Default.AfterStartApplication, Resources.ErrStartApplication); }
+
+            #endregion
+            this.Close();
+        }
+    }
+}
---
+++ version_updater/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("version_updater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ido")]
+[assembly: AssemblyProduct("version_updater")]
+[assembly: AssemblyCopyright("Copyright © ido 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b7153b60-c565-4267-accb-f1628e372c8f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
